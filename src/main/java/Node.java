2d3f1eb58fe4b195a87b51808b62c4db1f8f77e2
Jacob Chow <jjexpat00@ucla.edu>
2018-12-09 22:51:00 -0800

    Finish knapsack algo

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,6 +1,37 @@
 public class Node {
 
-    int level, profit, bound;
-    double weight;
+    private int level, profit, bound;
+    private static double weight;
 
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setProfit(int profit) {
+        this.profit = profit;
+    }
+
+    public void setBound(int bound) {
+        this.bound = bound;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getProfit() {
+        return profit;
+    }
+
+    public int getBound() {
+        return bound;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
